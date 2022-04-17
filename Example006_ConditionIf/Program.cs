ff09862594088e266e285796452b42c2084cb9ca
Alena <apispallada@gmail.com>
2022-04-17 19:07:31 +0300

    Example006 ConditionIfElse

---
+++ Example006_ConditionIf/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алёна")
+{
+    Console.WriteLine("Привет, человечек!"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
